7dde1691b45a40888575c07309539037b579dc49
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-20 11:16:37 -0300

    router entry components

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,9 +1,11 @@
 import styles from '../styles/Estiloso.module.css'
-
+import Link from 'next/link'
 export default function Estiloso(){
     return (
         <div className={styles.roxo}>
-            <h1>Estilo usando modulos</h1>
+            <Link href="/">
+                Voltar
+            </Link>
         </div>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 export default function Home(){
     return (
         <div>
-            <h1>Fundamentos do Next</h1>
+            <Link href="/estiloso">
+                estilo
+            </Link>
         </div>
     )
 }
