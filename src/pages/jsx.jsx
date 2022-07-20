fbd3f5eebb0450089e8c281aeb9df48f60d31b85
Ryan Gualberto <88859663+RyanGualberto@users.noreply.github.com>
2022-07-20 10:50:18 -0300

    s6a88:  using JSX

---
+++ src/pages/jsx.jsx
@@ -0,0 +1,10 @@
+export default function Jsx(){
+    const a = 3;
+    const b = 2;
+    return (
+        <div>
+            <h1>JSX e um conceito central</h1>
+            <h2>Mesclando html e js {a*b}</h2>
+        </div>
+    )
+}
